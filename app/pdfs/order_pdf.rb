e26432ebd75bb23816abf52317cbc9bbf1ebe5c4
Micah Nicodemus <micahnicodemus22@gmail.com>
2016-06-29 02:24:51 -0400

    Fix Up PDF

--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -10,12 +10,12 @@ class OrderPdf < Prawn::Document
 	end
 
 	def order_number
-		text "Order \##{@order.number}", size: 30, style: :bold
+		text "Order \##{@order.number}", size: 40, style: :bold, :align => :center
 	end
 
 	def line_items
-		move_down 20
-		table line_item_rows do
+		move_down 5
+		table line_item_rows, :position => :center do
 			row(0).font_style = :bold
 			columns(1..3).align = :right
 			self.row_colors = ["DDDDDD", "FFFFFF"]
@@ -35,7 +35,14 @@ class OrderPdf < Prawn::Document
 	end
 
 	def total_price
-		move_down 15
-		text "Total Price: #{price(@order.total_price)}", size: 16, style: :bold
+		@total_sum = 0
+		@order.line_items.each do |line_item|
+		@total_sum += line_item.full_price
+		move_down 5
+		end
+		text "Total Price: <color rgb='ff0000'>#{price(@total_sum)}</color>", size: 16, 
+																			  style: :bold,
+																			  :align => :center,
+																			  :inline_format => true
 	end
 end
